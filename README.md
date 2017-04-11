bb1591cf4ca25c07d0dbf77b8f48de4f913323a2
xudafeng <xudafeng@126.com>
2017-04-12 00:33:23 +0800

    add files

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 see: [bytedeco/javacv](//github.com/bytedeco/javacv)
 
+``` bash
+$ mvn -s settings.xml clean install
+```
+
 ## License
 
 The MIT License (MIT)
---
+++ src/java/sample/nodecv/Sample.java
@@ -0,0 +1,9 @@
+package sample.nodecv;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class Sample {
+	public static void main(String[] agrs) {
+		System.out.println("adfsds");
+	}
+}
